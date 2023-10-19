7f3e42a91113fed1ec6411356a451d96b07d4b3f
Stas Ryabets <stas.ryabets@mail.ru>
2023-10-19 13:06:25 +0500

    removed unnecessary imports

--- conftest.py
+++ conftest.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from pages.home_page import *
-from pages.order_page import *
-from locators import *
 import pytest
 
 
@@ -10,3 +7,5 @@ def driver():
     driver = webdriver.Firefox()
     yield driver
     driver.quit()
+
+    
